19fb41c64fb4514334f7c18653cd43bf7d2ecd9e
arifkm2006 <arifkmofficial@gmail.com>
2025-09-21 00:25:09 +0530

    refactor: remove unnecessary blank lines in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,10 +7,7 @@ import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { app,server} from './lib/socket.js';
 import path from "path";
-
-
 dotenv.config();
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -19,7 +16,6 @@ app.use(cors({
 }));
 app.use("/api/auth",router);
 app.use("/api/messages",messageRoutes);
-
 const PORT=process.env.PORT;
 const __dirname=path.resolve();
 
