218fd5705381372eefa420fce8fa26c2332766ec
arifkm2006 <arifkmofficial@gmail.com>
2025-10-06 18:28:46 +0530

    chore: update .gitignore and package files for improved dependency management

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
+import {config} from 'dotenv';
+config(); // to use .env file
 export const connectDB=async()=>{
   try{
     await mongoose.connect(process.env.MONGO_URL); // it is await as it takes some time
+    console.log("MongoDb connected successfully");
   }catch(error){
     console.log("MongoDb connection error: ",error);
   }
